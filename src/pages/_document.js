8209236e93135ecd11e3339f3b51ce0cc717972d
shaffanmohd <shaffanmohd@gmail.com>
2023-11-26 16:21:19 +0800

    add title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>LoveShaffanBasyirah</title>
+        <meta name="description" content="LoveShaffanBasyirah" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
