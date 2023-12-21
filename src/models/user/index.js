e3b4f6fc17db26c97e11fb53681acec708ff6dc0
shaffanmohd <shaffanmohd@gmail.com>
2023-12-21 09:55:00 +0800

    commnet out userschema

--- src/models/user/index.js
+++ src/models/user/index.js
@@ -1,11 +1,11 @@
-// models/user.js
-import mongoose from "mongoose";
+// // models/user.js
+// import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+// const userSchema = new mongoose.Schema({
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+// });
 
-const userSchema = mongoose.model("User", userSchema);
+// const userSchema = mongoose.model("User", userSchema);
 
-export default User;
+// export default User;
