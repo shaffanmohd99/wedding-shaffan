8cf68c473823d2a9a85dcf6cfa985e575f3780b2
shaffanmohd <shaffanmohd@gmail.com>
2024-02-20 23:37:34 +0800

    fix error

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -71,9 +71,9 @@ export default function HomePage() {
                 variant="body"
                 className="text-[#332117] text-center mt-2 "
               >
-                Dengan penuh rasa kesyukuran, sukacita menjemput Tan Sri | Dato'
-                Sri | Dato' | Datin | Tuan | Puan ke Majlis Perkahwinan puteri
-                kami bersama pasangannya
+                Dengan penuh rasa kesyukuran, sukacita menjemput Tan Sri |
+                Dato&apos; Sri | Dato&apos; | Datin | Tuan | Puan ke Majlis
+                Perkahwinan puteri kami bersama pasangannya
               </Typography>
               <div className={`${great_vibes.className}`}>
                 <Typography
